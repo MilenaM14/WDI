6269f73165635ed9858e975ad3b190f08ce9e327
MilenaM14 <milenamelaniuk@gmail.com>
2024-11-28 17:25:32 +0100

    Delete choinka.py

--- choinka.py
+++
@@ -1,9 +0,0 @@
-def main():
-    poziom=int(input("Podaj liczbe poziomów "))
-    for i in range(poziom):
-        for j in range(poziom-i-1):
-            print(" ",end="")
-        for k in range(2*i+1):
-            print("*",end="")
-        print()
-main()
