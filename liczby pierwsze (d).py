3c09d6e9237a699b626349b83bf385af988b46d1
MilenaM14 <milenamelaniuk@gmail.com>
2024-11-28 17:29:58 +0100

    Delete liczby pierwsze (d).py

--- liczby pierwsze (d).py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("Liczby pierwsze w zakresie od 2 do 99:")
-    for liczba in range(2,100):
-        for dzielnik in range(2,liczba):
-            if liczba%dzielnik==0:
-                break
-        else:
-            print(liczba,end=" ")
-main()
